31f702e1661eb697838b75505ae3aab25c370d06
Dev3 <Fernandonunez0304@gmail.com>
2020-06-10 03:02:27 -0400

    added app.js file

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    
+    var today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
+    
+
+    app.post("/", (req, res) => {
+        let item = req.body.newItem;
+
+        items.push(item);
+
+        res.redirect("/");
+    })
+});
+
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000");
+});
